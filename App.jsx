780bdc7e133660a2aeeb3257891d745eb04c9de8
Muhammad Ismail Shakir <160407658+MuhammadIsmailShakir2009@users.noreply.github.com>
2024-06-29 14:43:05 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import CarImages from "./carImages.jpeg"
+import SomePara from "./Para"
+
+
+const App = ()=>{
+  return(
+    <div>
+        <h1>Assignment no.5</h1>
+        <h1>Hello React!</h1>
+        <CarImages/>
+        <SomePara/>
+    </div>
+  )
+}
+
+
+export default App
---
+++ Para.jsx
@@ -0,0 +1,11 @@
+const Para = ()=>{
+    return(
+        <div>
+            <br></br>
+            <h1>This is Some Para</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br></br>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+
+export default Para
